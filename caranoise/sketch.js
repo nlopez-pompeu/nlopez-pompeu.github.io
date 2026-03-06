ded7f2e1166541d09b5226fd0f1b28bf2b6a93bc
nlopez-pompeu <nlopez@iespompeufabra.cat>
2026-03-06 10:25:33 +0100

    Update sketch.js

--- caranoise/sketch.js
+++ caranoise/sketch.js
@@ -4,20 +4,26 @@ function setup() {
 }
 
 function draw() {
-  background(156, 109, 242);//Color del fons del dibuix.
+  let r=random(0,127);
+  let g=random(0,127);
+  let b=random(128,255);
+  let x=random(0,50);
+  let y=100*noise(0.05*frameCount);
+  frameRate(5);
+  background(156-r, 109-g, 242-x);//Color del fons del dibuix.
   //El primer numero es el nivell de vermell"r", el segon número es el nivellde verd"g" i el tercer es l'intencitat de blau"B" per tant color RGB en html. Dintre de backgroud o fons.
   fill(255,246,229)//Color de la cara exterior
   //En el cas del fill serà al mateix pero el que fa es omplir de color la figura següent que son dos ellipses.
   ellipse(300,200,210,250);//Forma de la cara exterior
   //El primer número es la posició x del centre de l'ellipse desde la cantonada superior esquerra que es al "0,0", el segon número es la posició y" o l'alçada del centre de l'ellipsei el tercer numero es l'amplada de ellipse. El quart número  
   fill(255,250,250);//Color blanc dels ulls
- ellipse(265,160,30,25);//Formade l'ull dret perque x=160
+ ellipse(265,160,30+y,25+y);//Formade l'ull dret perque x=160
  fill(0,0,0)
   ellipse(265,160,15,13)
  fill(255, 255, 255)
-  ellipse(337,160,30,25);//Forma de l'ull esquerra perquè x=350
+  ellipse(337,160,30+y,25+y);//Forma de l'ull esquerra perquè x=350
   fill(0,0,0)
   ellipse(337,160,15,13)
  fill(255,110,110);//Color de la boca
-arc(300,240,90,50,0,PI);//Forma de la boca
+arc(300,240,90,50+x,0,PI);//Forma de la boca
 }
